78d3feeead17b37eb27cca2d6a4c6ecadfbdde75
Saeed Shahrivari <saeed.shahrivari@gmail.com>
2013-01-09 00:40:00 +0330

    Added sequential run for better profiling.

--- Rhino/MapRed/TextMapper.cs
+++ Rhino/MapRed/TextMapper.cs
@@ -19,6 +19,8 @@ namespace Rhino.MapRed
         protected Func<List<InterValue>, InterValue> combineFunc = null;
         protected TextInputReader reader;
         private Object diskLock=new object();
+
+        private string tempDirectory = @"c:\pashm";
         
         protected int maxChunkSize = 4 * 1024 * 1024;
         TimeSpan minWorkPeriod = new TimeSpan(0, 0, 0, 0, 100);
@@ -26,6 +28,10 @@ namespace Rhino.MapRed
         int maxCharsToMap = 32 * 1024 * 1024;
         int maxIntermediatePairsToSpill = 512 * 1024;
         int maxIntermediateFileSize = 256 * 1024 * 1024;
+
+        Dictionary<InterKey, List<InterValue>> cumulativeDictionary = new Dictionary<InterKey, List<InterValue>>(1024);
+        int cumulativeDicPairCount = 0;
+
 
         private static Logger logger = LogManager.GetCurrentClassLogger();
         
@@ -67,47 +73,27 @@ namespace Rhino.MapRed
             inputQ.CompleteAdding();
         }
 
-        private void combine()
+        private void doSpillIfNeeded(bool final_spill=false)
         {            
-            var cumolative_dic = new Dictionary<InterKey, List<InterValue>>(1024);
-            int cumolative_dic_pair_count = 0;
-            Stopwatch watch = new Stopwatch();
-
-            while (!dicsQ.IsCompleted)
+            if (cumulativeDictionary.Count>0 && (cumulativeDicPairCount > maxIntermediatePairsToSpill || final_spill))
             {
-                var dic = dicsQ.Take();
-                foreach(var pair in dic)
+                Stopwatch watch = new Stopwatch();
+                watch.Restart();
+                var sorted_pairs = cumulativeDictionary.AsParallel().OrderBy(t => t.Key).ToArray();
+                cumulativeDictionary = new Dictionary<InterKey, List<InterValue>>();
+                cumulativeDicPairCount = 0;
+                mapperInfo.SpilledRecords += sorted_pairs.Count();
+                watch.Stop();
+                logger.Debug("Sorted {0} records in {1}.", StringFormatter.DigitGrouped(sorted_pairs.Count()), watch.Elapsed);
+                IntermediateFile<InterKey, InterValue> inter_file = new IntermediateFile<InterKey, InterValue>(tempDirectory);
+                int written_bytes = 0;
+                lock (diskLock)
                 {
-                    List<InterValue> intermediate_list=null;
-                    if (!cumolative_dic.TryGetValue(pair.Key, out intermediate_list))
-                    {
-                         intermediate_list= new List<InterValue>();
-                         cumolative_dic[pair.Key]=intermediate_list;
-                    }
-
-                    var new_list = pair.Value;
-                    intermediate_list.AddRange(new_list);
-                    cumolative_dic_pair_count += new_list.Count;
-
-                    if (intermediate_list.Count > 4 && combineFunc != null)
-                    {
-                        cumolative_dic[pair.Key] = new List<InterValue>() { combineFunc.Invoke(intermediate_list) };
-                        cumolative_dic_pair_count -= intermediate_list.Count - 1;
-                    }
+                    written_bytes = inter_file.WriteRecords(sorted_pairs);
                 }
-
-                if (cumolative_dic_pair_count > maxIntermediatePairsToSpill)
+                mapperInfo.SpilledBytes += written_bytes;
+                if (!final_spill)
                 {
-                    watch.Restart();
-                    var sorted_pairs = cumolative_dic.AsParallel().OrderBy(t => t.Key).ToArray();
-                    cumolative_dic = new Dictionary<InterKey, List<InterValue>>();
-                    cumolative_dic_pair_count = 0;
-                    mapperInfo.SpilledRecords += sorted_pairs.Count();
-                    watch.Stop();
-                    logger.Debug("Sorted {0} records in {1}.", StringFormatter.DigitGrouped(sorted_pairs.Count()), watch.Elapsed);
-                    IntermediateFile<InterKey, InterValue> inter_file = new IntermediateFile<InterKey, InterValue>(@"z:\pashm\");
-                    int written_bytes=inter_file.WriteRecords(sorted_pairs);
-                    mapperInfo.SpilledBytes += written_bytes;
                     if (written_bytes < maxIntermediateFileSize)
                     {
                         maxIntermediatePairsToSpill = (int)(maxIntermediatePairsToSpill * (double)(maxIntermediateFileSize) / written_bytes);
@@ -120,63 +106,106 @@ namespace Rhino.MapRed
                     }
                 }
             }
-            if(cumolative_dic.Count>0)
-            {
-                watch.Restart();
-                var sorted_pairs = cumolative_dic.AsParallel().OrderBy(t => t.Key).ToArray();
-                cumolative_dic = new Dictionary<InterKey, List<InterValue>>();
-                cumolative_dic_pair_count = 0;
-                mapperInfo.SpilledRecords += sorted_pairs.Count();
-                watch.Stop();
-                logger.Debug("Sorted {0} records in {1}.", StringFormatter.DigitGrouped(sorted_pairs.Count()), watch.Elapsed);
-                IntermediateFile<InterKey, InterValue> inter_file = new IntermediateFile<InterKey, InterValue>(@"z:\pashm\");
-                int written_bytes = inter_file.WriteRecords(sorted_pairs);
-                mapperInfo.SpilledBytes += written_bytes;
-                logger.Debug("Finally spilling done in {0}.", watch.Elapsed);
-            }
+        }
 
-       
+        private void doCombine(Dictionary<InterKey, List<InterValue>> dic)
+        {
+            foreach (var pair in dic)
+            {
+                List<InterValue> intermediate_list = null;
+                if (!cumulativeDictionary.TryGetValue(pair.Key, out intermediate_list))
+                {
+                    intermediate_list = new List<InterValue>();
+                    cumulativeDictionary[pair.Key] = intermediate_list;
+                }
+
+                var new_list = pair.Value;
+                intermediate_list.AddRange(new_list);
+                cumulativeDicPairCount += new_list.Count;
+
+                if (intermediate_list.Count > 4 && combineFunc != null)
+                {
+                    cumulativeDictionary[pair.Key] = new List<InterValue>() { combineFunc.Invoke(intermediate_list) };
+                    cumulativeDicPairCount -= intermediate_list.Count - 1;
+                }
+            }
+        }
+
+        private void combine()
+        {            
+            Stopwatch watch = new Stopwatch();
+
+            while (!dicsQ.IsCompleted)
+            {
+                var dic = dicsQ.Take();
+                doCombine(dic);
+                doSpillIfNeeded();
+            }
+            doSpillIfNeeded(true);
+            //if(cumulativeDictionary.Count>0)
+            //{
+            //    watch.Restart();
+            //    var sorted_pairs = cumulativeDictionary.AsParallel().OrderBy(t => t.Key).ToArray();
+            //    cumulativeDictionary = new Dictionary<InterKey, List<InterValue>>();
+            //    cumulativeDicPairCount = 0;
+            //    mapperInfo.SpilledRecords += sorted_pairs.Count();
+            //    watch.Stop();
+            //    logger.Debug("Sorted {0} records in {1}.", StringFormatter.DigitGrouped(sorted_pairs.Count()), watch.Elapsed);
+            //    IntermediateFile<InterKey, InterValue> inter_file = new IntermediateFile<InterKey, InterValue>(tempDirectory);
+            //    int written_bytes = 0;
+            //    lock (diskLock)
+            //    {
+            //        written_bytes = inter_file.WriteRecords(sorted_pairs);
+            //    }
+            //    mapperInfo.SpilledBytes += written_bytes;
+            //    logger.Debug("Finally spilling done in {0}.", watch.Elapsed);
+            //}       
+        }
+
+
+        private IEnumerable<Dictionary<InterKey, List<InterValue>>> doMap(InputTextCunk chunk, int thread_num=0)
+        {
+            Stopwatch watch = new Stopwatch();
+            var input_records = chunk.Records;
+            var char_count = chunk.CharCount;
+
+            watch.Restart();
+            var dics = new ConcurrentBag<Dictionary<InterKey, List<InterValue>>>();
+            ParallelOptions option = new ParallelOptions();
+            if (thread_num == 0)
+                option.MaxDegreeOfParallelism = Environment.ProcessorCount * 2;
+            else
+                option.MaxDegreeOfParallelism = thread_num;
+            Parallel.ForEach(Partitioner.Create(0, input_records.Count), option, (range) =>
+            {
+                var dic = new Dictionary<InterKey, List<InterValue>>();
+                var context = new MapContext<InterKey, InterValue>(dic);
+                for (int i = range.Item1; i < range.Item2; i++)
+                    mapFunc.Invoke(input_records[i], context);
+                dics.Add(dic);
+                Interlocked.Add(ref mapperInfo.MapEmits, context.EmitCount);
+            });
+            watch.Stop();
+
+            mapperInfo.ProcessedRecords += input_records.Count;
+            mapperInfo.ProcessedChars += char_count;
+
+            if (watch.Elapsed > maxWorkPeriod)
+                maxChunkSize = Math.Min(maxChunkSize / 2, maxCharsToMap);
+            if (watch.Elapsed < minWorkPeriod)
+                maxChunkSize = Math.Min(maxChunkSize * 2, maxCharsToMap);
+
+            logger.Debug("Mapped a chunk with {0} chars in {1}", StringFormatter.DigitGrouped(char_count), watch.Elapsed);
+            return dics;
         }
 
         private void consumeInput(int thread_num = 0)
         {
-            Stopwatch watch = new Stopwatch();
             while (!inputQ.IsCompleted)
             {
                 
                 var chunk=inputQ.Take();
-                var input_records = chunk.Records;
-                var char_count = chunk.CharCount;
-                //Console.WriteLine("Got Chunk!");
-
-                watch.Restart();
-                var dics = new ConcurrentBag<Dictionary<InterKey, List<InterValue>>>();
-                ParallelOptions option = new ParallelOptions();
-                if (thread_num == 0)
-                    option.MaxDegreeOfParallelism = Environment.ProcessorCount * 2;
-                else
-                    option.MaxDegreeOfParallelism = thread_num;
-                Parallel.ForEach(Partitioner.Create(0, input_records.Count), option, (range) =>
-                {
-                    var dic = new Dictionary<InterKey, List<InterValue>>();
-                    var context = new MapContext<InterKey, InterValue>(dic);
-                    for (int i = range.Item1; i < range.Item2; i++)
-                        mapFunc.Invoke(input_records[i], context);
-                    dics.Add(dic);
-                    Interlocked.Add(ref mapperInfo.MapEmits, context.EmitCount);
-                });
-                watch.Stop();
-
-                mapperInfo.ProcessedRecords += input_records.Count;
-                mapperInfo.ProcessedChars+= char_count;
-
-                if (watch.Elapsed > maxWorkPeriod)
-                    maxChunkSize = Math.Min(maxChunkSize / 2,maxCharsToMap);
-                if (watch.Elapsed < minWorkPeriod)
-                    maxChunkSize = Math.Min(maxChunkSize * 2,maxCharsToMap);
-                
-                logger.Debug("Mapped a chunk with {0} chars in {1}", StringFormatter.DigitGrouped(char_count), watch.Elapsed);
-
+                var dics = doMap(chunk, thread_num);
                 foreach (var dic in dics)
                     dicsQ.Add(dic);
             }
@@ -204,7 +233,37 @@ namespace Rhino.MapRed
             logger.Info("Mapper mapped {0} records that sums to {1} chars.", StringFormatter.DigitGrouped(mapperInfo.ProcessedRecords), StringFormatter.HumanReadablePostfixs(mapperInfo.ProcessedChars));
             logger.Info("Mapper emmited {0} pairs.", StringFormatter.DigitGrouped(mapperInfo.MapEmits));
             logger.Info("Mapper spilled {0} records that sums to {1} bytes.", StringFormatter.DigitGrouped(mapperInfo.SpilledRecords), StringFormatter.HumanReadablePostfixs(mapperInfo.SpilledBytes));
+        }
 
+        public void SequentialRun()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            while (true)
+            {
+                InputTextCunk input_chunk;
+                int char_count = 0;
+                lock (diskLock)
+                {
+                    char_count = reader.ReadChunk(out input_chunk, maxChunkSize);
+                }
+                if (char_count == 0)
+                    break;
+                
+                logger.Debug("Read a chunk: {0} records and {1} chars. InputQ count is {2}", StringFormatter.DigitGrouped(input_chunk.CharCount), StringFormatter.HumanReadablePostfixs(char_count), inputQ.Count);
+                var dics=doMap(input_chunk);
+                foreach (var dic in dics)
+                {
+                    doCombine(dic);
+                    doSpillIfNeeded();
+                }                
+            }
+            doSpillIfNeeded(true);
+            watch.Stop();
+            logger.Info("Mapper finished the job in {0}!", watch.Elapsed);
+            logger.Info("Mapper mapped {0} records that sums to {1} chars.", StringFormatter.DigitGrouped(mapperInfo.ProcessedRecords), StringFormatter.HumanReadablePostfixs(mapperInfo.ProcessedChars));
+            logger.Info("Mapper emmited {0} pairs.", StringFormatter.DigitGrouped(mapperInfo.MapEmits));
+            logger.Info("Mapper spilled {0} records that sums to {1} bytes.", StringFormatter.DigitGrouped(mapperInfo.SpilledRecords), StringFormatter.HumanReadablePostfixs(mapperInfo.SpilledBytes));
         }
     }
 }
